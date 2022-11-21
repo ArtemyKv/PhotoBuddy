e24be892fccf750b95a776084520aa0f5a6586e7
ArtemyKv <artemykv@gmail.com>
2022-11-21 23:09:10 +0500

    add custom description to location model

--- PhotoBuddy/Model/Location.swift
+++ PhotoBuddy/Model/Location.swift
@@ -11,3 +11,19 @@ struct Location: Codable {
     var city: String?
     var country: String?
 }
+
+extension Location: CustomStringConvertible {
+    var description: String {
+        if let city = city, let country = country {
+            return "\(city), \(country)"
+        } else if let city = city, country == nil {
+            return "\(city)"
+        } else if let country = country, city == nil {
+            return "\(country)"
+        } else {
+            return " - "
+        }
+    }
+    
+    
+}
