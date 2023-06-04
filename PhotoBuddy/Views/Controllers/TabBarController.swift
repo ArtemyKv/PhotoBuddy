cdb1136dd40f54f08ffe49cc2001af215727798e
ArtemKvashnin <artemykv@gmail.com>
2023-06-04 14:18:39 +0500

    refactor TabBarController

--- PhotoBuddy/Views/Controllers/TabBarController.swift
+++ PhotoBuddy/Views/Controllers/TabBarController.swift
@@ -8,28 +8,31 @@
 import Foundation
 import UIKit
 
-class TabBarController: UITabBarController {
+final class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        createChildControllers()
-        self.tabBar.tintColor = .label
-        self.tabBar.backgroundColor = .systemBackground
-
+        configureTabBar()
+        makeChildControllers()
     }
     
-    func createChildControllers() {
+    private func makeChildControllers() {
         let searchPhotosVC = SearchResultsViewController()
         let firstTabNavigationController = UINavigationController(rootViewController: searchPhotosVC)
         
         let favoritePhotosVC = FavoritePhotosViewController()
         let secondTabNavigationController = UINavigationController(rootViewController: favoritePhotosVC)
-        self.viewControllers = [firstTabNavigationController, secondTabNavigationController]
+        viewControllers = [firstTabNavigationController, secondTabNavigationController]
         
         let firstTabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 0)
         let secondTabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "star"), tag: 0)
         
         firstTabNavigationController.tabBarItem = firstTabBarItem
         secondTabNavigationController.tabBarItem = secondTabBarItem
+    }
+    
+    private func configureTabBar() {
+        tabBar.tintColor = .label
+        tabBar.backgroundColor = .systemBackground
     }
 }
